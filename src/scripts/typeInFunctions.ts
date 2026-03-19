465d1fad2c5cc5f87497445a25a7360ef8fa71a1
Anuj <anuj89806@gmail.com>
2026-03-19 15:25:27 +0530

    rest, optional and default parameters in function

--- src/scripts/typeInFunctions.ts
+++ src/scripts/typeInFunctions.ts
@@ -30,3 +30,58 @@ function checkNames(name: string): boolean {
 }
 
 console.log(checkNames("peter"));
+
+// optional parameters
+function calculatePrice(price: number, discount?: number): number {
+  return price - (discount || 50);
+}
+
+let priceAfterDiscount = calculatePrice(1000, 100);
+
+// default parameters
+function calcScore(initialScore: number, penaltyPoints: number = 20): number {
+  return initialScore - penaltyPoints;
+}
+
+let scoreAfterPenalty = calcScore(50, 10);
+let scoreWithoutPenalty = calcScore(200);
+
+// rest parameters
+function sum(message: string, ...numbers: number[]): string {
+  const doubled = numbers.map((num) => num * 2);
+
+  const total = numbers.reduce((acc, cur) => {
+    return acc + cur;
+  }, 0);
+
+  return `${message}${total}`;
+}
+
+let result = sum("The total is: ", 15, 2, 6, 4, 5);
+console.log(result);
+
+// type Void
+function logMessage(message: string): void {
+  console.log(message);
+}
+
+logMessage("hello, typescript");
+
+// ## Functions - Using Union Types as Function Parameters
+
+// ### Challenge
+
+// Your task is to create a function named processInput that accepts a parameter of a union type string | number. The function should behave as follows:
+
+// - If the input is of type number, the function should multiply the number by 2 and log the result to the console.
+// - If the input is of type string, the function should convert the string to uppercase and log the result to the console.
+
+function processInput(input: string | number) {
+  if (typeof input === "string") {
+    console.log(input.toUpperCase());
+  } else {
+    console.log(input * 2);
+  }
+}
+processInput(2);
+processInput("hey there");
