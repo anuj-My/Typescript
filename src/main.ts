80b388476aa47282eeb590cc100b8c595c76e513
Anuj <anuj89806@gmail.com>
2026-03-19 13:14:05 +0530

    type annotations

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import "./style.css";
-import "./scripts/typeAnnotations.js";
+import "./scripts/typeAnnotations.ts";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
 <h1>TypeScript</h1>
--- src/scripts/typeAnnotations.ts
+++ src/scripts/typeAnnotations.ts
@@ -1,3 +1,36 @@
 console.log("typescript");
 
-let movie:string = 'spiderman'
+// type string
+let movie: string = "spiderman";
+movie = "batman";
+
+movie = movie.toUpperCase();
+console.log(movie);
+
+// type number
+let amount: number = 20;
+amount = 12 - 1;
+
+// type boolean
+let isActive: boolean = false;
+isActive = true;
+
+// ## Challenge
+
+// - Create a variable of type string and try to invoke a string method on it.
+// - Create a variable of type number and try to perform a mathematical operation on it.
+// - Create a variable of type boolean and try to perform a logical operation on it.
+// - Try to assign a value of a different type to each of these variables and observe the TypeScript compiler's response.
+// - You can use type annotation or inference
+
+const firstName: string = "Peter";
+
+const fullName: string = firstName.concat("Parker");
+console.log(fullName);
+
+let age: number = 26;
+age = age + 5;
+
+let isAdult: boolean = age >= 18;
+
+console.log(isAdult);
